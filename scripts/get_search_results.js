d420d4eb4ab02de4226bba9a87980f3a1471555c
Sohaib <sohaibk321@gmail.com>
2021-02-10 16:50:15 -0500

    initial import

---
+++ scripts/get_search_results.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("#men").click(function() {
+    $(".page_content").load("./pages/men.html")
+  });
+
+  $("#women").click(function() {
+    $(".page_content").load("./pages/women.html")
+  });
+
+  $("#climb").click(function() {
+    $(".page_content").load("./pages/climb.html")
+  });
+
+  $("#camp").click(function() {
+    $(".page_content").load("./pages/camp.html")
+  });
+
+  $("#cycle").click(function() {
+    $(".page_content").load("./pages/cycle.html")
+  });
+
+  $("#snow").click(function() {
+    $(".page_content").load("./pages/snow.html")
+  });
+
+  $("#more").click(function() {
+    $(".page_content").load("./pages/more.html")
+  });
+});
